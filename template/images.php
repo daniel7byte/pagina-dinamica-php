aeb54a3a20d6660cca1241e142004d215b6c9d7c
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2020-04-30 18:44:29 -0500

    Mejor generador de imagenes

--- template/images.php
+++ template/images.php
@@ -1,21 +1,27 @@
 <?php
 
-  function listarArchivos( $path ){
-    // Abrimos la carpeta que nos pasan como parámetro
-    $dir = opendir($path);
-    // Leo todos los ficheros de la carpeta
-    while ($elemento = readdir($dir)){
-        // Tratamos los elementos . y .. que tienen todas las carpetas
-        if( $elemento != "." && $elemento != ".."){
-            // Si es un archivo lo muestra
-            if( !is_dir($path.$elemento) ){
-                // Muestro el fichero
-                echo '<img src="' . $path . $elemento . '" width="300px">';
-                echo '<br>';
-            }
-        }
+  $path = "images/";
+  // Abrimos la carpeta que nos pasan como parámetro
+  $dir = opendir($path);
+  // Leo todos los ficheros de la carpeta
+  while ($elemento = readdir($dir)){
+    // Tratamos los elementos . y .. que tienen todas las carpetas
+    if( $elemento != "." && $elemento != ".."){
+      // Si es un archivo lo muestra
+      if( !is_dir($path.$elemento) ){
+        // Muestro el fichero
+?>
+
+<!-- El codigo HTML inicia aquí -->
+
+
+<img src="<?= $path.$elemento ?>" width="300"><br>
+
+
+<!-- El codigo HTML termina aquí -->
+
+<?php
+      }
     }
   }
-
-  // Llamamos a la función para que nos muestre el contenido de la carpeta gallery
-  listarArchivos("images/");
+?>
