c3c43afca0fa42eb4f9d90c6466e2af6bd9fc114
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2020-04-30 17:10:37 -0500

    Producto Dinamico

---
+++ producto.php
@@ -0,0 +1,18 @@
+<?php $imagen = $_GET['img']; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+
+  <h1>Imagen <?=$imagen?></h1>
+
+  <img src="<?=$imagen?>" alt="">
+
+</body>
+</html>
